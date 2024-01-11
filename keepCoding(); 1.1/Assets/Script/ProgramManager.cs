e5cdd53c1f20d893b7a03819daa3b5302696997b
Frenz R <125328585+ChimaRyder@users.noreply.github.com>
2024-01-11 21:12:47 +0800

    Added Browser, program implementation
    Added Browser, program implementation

--- keepCoding(); 1.1/Assets/Script/ProgramManager.cs
+++ keepCoding(); 1.1/Assets/Script/ProgramManager.cs
@@ -8,18 +8,28 @@ public class ProgramManager : MonoBehaviour
     [SerializeField] private List<Program> programs = new List<Program>();
 
     public void openProgram(string openID) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < programs.Count; i++) {
             if (programs[i].windowID == openID){
-                Debug.Log(programs[i]);
+                if (programs[i].window.activeSelf)
+                {
+                    programs[i].window.SetActive(false);
+                    break;
+                }
+                
+                programs[i].window.SetActive(true);
+            }
+            else
+            {
+                programs[i].window.SetActive(false);
             }
         }
 
     }
 
     public void closeProgram(string closeID) {
-            for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             if (programs[i].windowID == closeID){
-                //close the program
+                programs[i].window.SetActive(false);
             }
         }
     }
